505a2bfe95702356e758da1aa9d188f7cc0fc3d9
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-03-14 01:11:13 -0500

    Add program section

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-const test = document.getElementById('test');
+//  Mobile Menu
 
-test.addEventListener('click', () => {
-  test.style.color = 'blue';
+const menuButton = document.querySelector('#menuButton');
+
+menuButton.addEventListener('click', () => {
+  document.getElementById('menu').style.display = 'block';
 });
+
+//  Program cards data
+
+const program = [
+  {
+    image: 'assets/programIcon1.svg',
+    activity: 'Lecture',
+    brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
+  },
+  {
+    image: 'assets/programIcon1.svg',
+    activity: 'Exhibition',
+    brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
+  },
+  {
+    image: 'assets/programIcon1.svg',
+    activity: 'Forum',
+    brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
+  },
+  {
+    image: 'assets/programIcon1.svg',
+    activity: 'Workshop',
+    brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
+  },
+  {
+    image: 'assets/programIcon1.svg',
+    activity: 'Test',
+    brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
+  },
+];
+
+// Program Section Card Creator
+
+const cardContainer = document.querySelector('#programContainer');
+
+for (let i = 0; i < program.length; i += 1) {
+  const programCard = `
+  <div class="programCard">
+    <img src=${program[i].image} alt="#" class="programImage">
+    <p class="programActivity">${program[i].activity}</p>
+    <p class="programBrief">${program[i].brief}</p>
+  </div>
+  `;
+  cardContainer.innerHTML += programCard;
+};
