4c2a34444daa2a8a2036093acbb0f38dbd2beaf4
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-03-15 00:09:38 -0500

    Add about page

--- script.js
+++ script.js
@@ -15,22 +15,22 @@ const program = [
     brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
   },
   {
-    image: 'assets/programIcon1.svg',
+    image: 'assets/programIcon2.svg',
     activity: 'Exhibition',
     brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
   },
   {
-    image: 'assets/programIcon1.svg',
+    image: 'assets/programIcon3.svg',
     activity: 'Forum',
     brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
   },
   {
-    image: 'assets/programIcon1.svg',
+    image: 'assets/programIcon4.svg',
     activity: 'Workshop',
     brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
   },
   {
-    image: 'assets/programIcon1.svg',
+    image: 'assets/programIcon5.svg',
     activity: 'Test',
     brief: 'Mauris non urna non neque imperdiet un mattis. Cras maximus pulvinar sagittis. Morbi euismod luctus ex quis porta.',
   },
@@ -50,3 +50,90 @@ for (let i = 0; i < program.length; i += 1) {
   `;
   cardContainer.innerHTML += programCard;
 };
+
+// Speakers Info
+
+const speakers = [
+  {
+    class: 'speakerCard',
+    image: 'assets/speaker6.svg',
+    name: 'Juan Perez',
+    position: 'N.Y Firemen Security Chief',
+    brief: 'Aliquam vel porta sapien, sit amet vestibulum nisl. Aliquam vel porta sapien, sit amet vestibulum nisl.',
+  },
+  {
+    class: 'speakerCard',
+    image: 'assets/speaker5.svg',
+    name: 'Margareth Jones',
+    position: 'N.Y Firemen Security Chief',
+    brief: 'Aliquam vel porta sapien, sit amet vestibulum nisl. Aliquam vel porta sapien, sit amet vestibulum nisl.',
+  },
+  {
+    class: 'speakerCard',
+    image: 'assets/speaker4.svg',
+    name: 'Chio Ming',
+    position: 'N.Y Firemen Security Chief',
+    brief: 'Aliquam vel porta sapien, sit amet vestibulum nisl. Aliquam vel porta sapien, sit amet vestibulum nisl.',
+  },
+  {
+    class: 'speakerCard',
+    image: 'assets/speaker3.svg',
+    name: 'Ameed Dassum',
+    position: 'N.Y Firemen Security Chief',
+    brief: 'Aliquam vel porta sapien, sit amet vestibulum nisl. Aliquam vel porta sapien, sit amet vestibulum nisl.',
+  },
+  {
+    class: 'speakerCard',
+    image: 'assets/speaker2.svg',
+    name: 'Sarah Glass',
+    position: 'N.Y Firemen Security Chief',
+    brief: 'Aliquam vel porta sapien, sit amet vestibulum nisl. Aliquam vel porta sapien, sit amet vestibulum nisl.',
+  },
+  {
+    class: 'speakerCard',
+    image: 'assets/speaker1.svg',
+    name: 'Frank Williams',
+    position: 'N.Y Firemen Security Chief',
+    brief: 'Aliquam vel porta sapien, sit amet vestibulum nisl. Aliquam vel porta sapien, sit amet vestibulum nisl.',
+  },
+]
+
+// Speakers Section Card Creator
+
+const speakerContainer = document.querySelector('#speakersContainer');
+const moreSpeakersContainer = document.querySelector('#moreSpeakersContainer');
+
+for (let i = 0; i < speakers.length; i += 1) {
+  const speakerCard = `
+  <div class="${speakers[i].class}">
+    <img src=${speakers[i].image} alt="speaker photo" class="speakerImage">
+    <div class="speakerInfo">
+      <h5>${speakers[i].name}</h5>
+      <p class="speakerPosition">${speakers[i].position}</p>
+      <hr>
+      <p class="speakerBrief">${speakers[i].brief}</p>
+    </div>
+  </div>
+  `;
+  if (i < 2) {
+    speakerContainer.innerHTML += speakerCard;
+  } else {
+    moreSpeakersContainer.innerHTML += speakerCard;
+  }
+  
+};
+
+// More speakers mobile section
+
+const moreSpeakerBtn = document.querySelector('.moreSpeakerBtn');
+
+moreSpeakerBtn.addEventListener('click', () => {
+  const x = document.getElementById('moreSpeakersContainer');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    moreSpeakerBtn.innerHTML = 'MORE <img src="assets/arrowMore.svg" alt="arrow" class="moreBtnArrow">'
+  } else {
+    x.style.display = 'block'
+    moreSpeakerBtn.innerHTML = 'LESS  <img src="assets/arrowMore.svg" alt="arrow" class="lessBtnArrow">'
+  }
+});
